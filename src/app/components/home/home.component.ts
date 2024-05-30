2fe40d2e946511c86754243e285d773ef4762f1f
reffcp_ultratech <crefugio-ext@ultratech-inc.com>
2024-05-30 08:42:42 -0600

    style: :lipstick: ajuste para implementar darkmode

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  activeDarkMode = false;
 
+  ngOnInit() {
+    const darkMode = localStorage.getItem('dark-mode');
+    if (darkMode === 'true') {
+      document.body.classList.add('dark-mode');
+      this.activeDarkMode = true;
+    }
+  }
+
+  toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    const darkMode = document.body.classList.contains('dark-mode');
+    localStorage.setItem('dark-mode', darkMode.toString());
+    this.activeDarkMode = darkMode;
+  }
 }
