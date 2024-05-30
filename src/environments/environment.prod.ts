a60701da2dc89cf4616e749315eed753e064542c
reffcp_ultratech <crefugio-ext@ultratech-inc.com>
2024-05-30 11:26:40 -0600

    feat: :sparkles: se integra config inicial de firebase para despliegues y uso de realtime

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
-}
+  firebaseConfig: {
+    apiKey: 'AIzaSyAu6zprKoqshRPqfCiyO7xkqeHSOlIO5Bk',
+    authDomain: 'portafolio-personal-264fb.firebaseapp.com',
+    databaseURL: 'https://portafolio-personal-264fb.firebaseio.com',
+    projectId: 'portafolio-personal-264fb',
+    storageBucket: 'portafolio-personal-264fb.appspot.com',
+    messagingSenderId: '798166672425',
+    appId: '1:798166672425:web:2ff0a2ea8fb843fa1e2838',
+  },
+};
