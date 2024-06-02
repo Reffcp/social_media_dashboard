0358af2fe300adcfb6daff249b9d3125c1a5e943
reffcp_ultratech <crefugio-ext@ultratech-inc.com>
2024-06-01 22:28:53 -0600

    feat: :sparkles: se agregan cards minis de page views y likes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardSocialMediaComponent } from './components/card-social-media/card-social-media.component';
+import { CardLikesComponent } from './components/card-likes/card-likes.component';
 
 @NgModule({
-  declarations: [CardSocialMediaComponent],
+  declarations: [CardSocialMediaComponent, CardLikesComponent],
   imports: [CommonModule],
-  exports: [CardSocialMediaComponent],
+  exports: [CardSocialMediaComponent, CardLikesComponent],
 })
 export class SharedModule {}
